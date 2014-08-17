781de718639351c76d472b12d3d3cc0dfddbe46d
liamjbennett <lbennett@opentable.com>
2014-08-17 13:44:51 +0100

    fixing bug in host_entry method

--- lib/beaker/dsl/install_utils.rb
+++ lib/beaker/dsl/install_utils.rb
@@ -601,7 +601,7 @@ module Beaker
           powershell_pre = "powershell.exe -InputFormat None -NoProfile -NonInteractive -NoLogo -ExecutionPolicy Bypass"
           hosts_file = "C:\\Windows\\System32\\Drivers\\etc\\hosts"
           host_entry = "#{opts['ip']}`t`t#{opts['name']}"
-          on host, "#{powershell_pre} -Command \"\$text = \\\"#{host_entry}\\\"; Set-Content -path '#{hosts_file}' -value \$text\""
+          on host, "#{powershell_pre} -Command \"\$text = \\\"#{host_entry}\\\"; Add-Content -path '#{hosts_file}' -value \$text\""
         end
       end
 
